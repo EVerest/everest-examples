db1bb7be0313802407bfa205d89ffc16c790d49d
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2026-01-22 00:06:41 +0100

    Add dependencies to everest::gpio and ::io libraries to demonstrate usage
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- modules/ExamplePublisher/ExamplePublisher.cpp
+++ modules/ExamplePublisher/ExamplePublisher.cpp
@@ -2,10 +2,16 @@
 // Copyright Pionix GmbH and Contributors to EVerest
 #include "ExamplePublisher.hpp"
 
+#include <everest/gpio/gpio.hpp>
+#include <everest/io/event/fd_event_handler.hpp>
+
 namespace module {
 
 void ExamplePublisher::init() {
     invoke_init(*p_example_pub);
+
+    Everest::Gpio test_gpio;
+    everest::lib::io::event::fd_event_handler ev_handler;
 }
 
 void ExamplePublisher::ready() {
